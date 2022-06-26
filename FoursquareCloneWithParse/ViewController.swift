04ca047d21f1c5c7b071bbd96f9af0b1f0dc7752
ergeakova <ergeak@gmail.com>
2022-06-26 15:35:57 +0300

    Initial Commit

---
+++ FoursquareCloneWithParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareCloneWithParse
+//
+//  Created by Erge Gevher Akova on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

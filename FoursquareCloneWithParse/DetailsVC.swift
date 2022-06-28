800756ad1261a2d0471ab05b4695742285c89c39
ergeakova <ergeak@gmail.com>
2022-06-29 01:29:57 +0300

    Added navigation to the map

--- FoursquareCloneWithParse/DetailsVC.swift
+++ FoursquareCloneWithParse/DetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, MKMapViewDelegate {
 
     var utl = Utils()
     @IBOutlet weak var detailsImage: UIImageView!
@@ -24,6 +24,7 @@ class DetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        detailsMap.delegate = self
         getData()
     }
     
@@ -88,5 +89,42 @@ class DetailsVC: UIViewController {
         anotation.title = self.lblPlaceName.text!
         anotation.subtitle = self.lblPlaceType.text!
         self.detailsMap.addAnnotation(anotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        }else{
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenlatitude != 0.0 && self.chosenlongitude != 0.0 {
+            let requestLocation = CLLocation(latitude: self.chosenlatitude, longitude: self.chosenlongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation){ (placeMarks, error) in
+                if let placeMark = placeMarks {
+                    if placeMark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placeMark[0])
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.lblPlaceName.text
+                        let lounchoptions = [ MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                        mapItem.openInMaps(launchOptions: lounchoptions)
+                    }
+                }
+            }
+        }
     }
 }
